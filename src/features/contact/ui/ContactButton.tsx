aba8d1fd3b8631e54c5eb517449f612e33322e71
GuDoYoon <rhehfl418q@gmail.com>
2025-08-07 00:11:55 +0900

    (#5): 문의하기 버튼 생성

---
+++ src/features/contact/ui/ContactButton.tsx
@@ -0,0 +1,7 @@
+export default function ContactButton() {
+  return (
+    <button className="px-4 py-2 bg-[#007DA2] text-white rounded-[10px] cursor-pointer">
+      문의하기
+    </button>
+  );
+}
--- src/features/lecture-detail/ui/DetailHeader.tsx
+++ src/features/lecture-detail/ui/DetailHeader.tsx
@@ -1,3 +1,5 @@
+import { ContactButton } from '@/features/contact';
+
 export default function DetailHeader() {
   return (
     <header className="w-full px-19 py-5 bg-[#005873] h-50">
@@ -15,11 +17,11 @@ export default function DetailHeader() {
             d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
           />
         </svg>
-        <ul className="h-full flex flex-col justify-between py-5 text-white">
+        <ul className="h-full flex flex-col justify-between py-5 text-white ml-5">
           <li>
             <h1 className="text-4xl font-bold">웹 프론트엔드 기초</h1>
           </li>
-          <li className="flex text-xl items-center">
+          <li className="flex text-xl items-center font-bold">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -34,7 +36,9 @@ export default function DetailHeader() {
                 d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
               />
             </svg>
-            <span>김동현 교수</span> |<span>문의하기</span>
+            <span className="mx-2.5">김동현 교수</span>
+            <div className="w-px h-6 bg-white mr-2.5" />
+            <ContactButton />
           </li>
         </ul>
       </div>
