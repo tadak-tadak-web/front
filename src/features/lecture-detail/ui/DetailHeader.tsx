a7413b4008e6c4d66bc5f9c531be342e613ffce5
GuDoYoon <rhehfl418q@gmail.com>
2025-08-08 02:51:55 +0900

    (#5): heroIcons 추가

--- src/features/lecture-detail/ui/DetailHeader.tsx
+++ src/features/lecture-detail/ui/DetailHeader.tsx
@@ -1,10 +1,11 @@
 import { ContactButton } from '@/features/contact';
+import { UserCircleIcon, UserIcon } from '@heroicons/react/20/solid';
 
 export default function DetailHeader() {
   return (
     <header className="w-full lg:px-19 lg:py-5 bg-[#005873] h-25 lg:h-50">
       <div className="flex items-center h-full">
-        <ProfileFallbackImage className="size-20 lg:size-40" />
+        <UserCircleIcon className="size-20 lg:size-40" fill="white" />
         <ul className="h-full flex flex-col justify-between py-2 lg:py-5 text-white ml-5">
           <li>
             <h1 className="text-2xl lg:text-4xl font-bold">
@@ -12,7 +13,7 @@ export default function DetailHeader() {
             </h1>
           </li>
           <li className="flex text-sm lg:text-xl items-center font-bold">
-            <ProfileIcon />
+            <UserIcon className="size-5" />
             <span className="mx-2.5">김동현 교수</span>
             <div className="w-px h-6 bg-white mr-2.5" />
             <ContactButton />
@@ -20,42 +21,5 @@ export default function DetailHeader() {
         </ul>
       </div>
     </header>
-  );
-}
-
-function ProfileFallbackImage({ className }: { className?: string }) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke="currentColor"
-      className={className}
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
-      />
-    </svg>
-  );
-}
-
-function ProfileIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={1.5}
-      stroke="currentColor"
-      className="size-6"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
-      />
-    </svg>
   );
 }
