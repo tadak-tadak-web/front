1331d3c7d0e5368b964bb4db1cbf0e272db9a79d
GuDoYoon <rhehfl418q@gmail.com>
2025-08-12 21:28:03 +0900

    (#13): 모킹 api 추가

---
+++ src/mocks/auth.mock.ts
@@ -0,0 +1,41 @@
+import { http, HttpResponse } from 'msw';
+
+// 로그인 핸들러
+export const authHandlers = [
+  http.post<object, { username: string; password: string }>(
+    '/login',
+    async ({ request }) => {
+      const { username, password } = await request.json();
+
+      if (username === 'john' && password === 'secret') {
+        return HttpResponse.json(
+          { message: 'Login successful' },
+          {
+            headers: {
+              'Set-Cookie':
+                'sessionId=abc123; Path=/; HttpOnly; Secure; SameSite=Strict',
+            },
+          }
+        );
+      }
+
+      return HttpResponse.json(
+        { message: 'Invalid credentials' },
+        { status: 401 }
+      );
+    }
+  ),
+
+  // 로그인된 유저 정보 요청
+  http.get('/user', ({ cookies }) => {
+    if (cookies.sessionId === 'abc123') {
+      return HttpResponse.json({
+        id: 'abc-123',
+        firstName: 'John',
+        lastName: 'Maverick',
+      });
+    }
+
+    return HttpResponse.json({ message: 'Unauthorized' }, { status: 401 });
+  }),
+];
