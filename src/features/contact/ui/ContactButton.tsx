8bafff030c27c65cc931df0067bdb97c3fc54b26
GuDoYoon <rhehfl418q@gmail.com>
2025-08-12 19:17:01 +0900

    (#9): 기본 테마 지정

--- src/features/contact/ui/ContactButton.tsx
+++ src/features/contact/ui/ContactButton.tsx
@@ -1,6 +1,6 @@
 export default function ContactButton() {
   return (
-    <button className="px-4 py-2 bg-[#007DA2] text-white rounded-[10px] cursor-pointer">
+    <button className="px-4 py-2 bg-blue-300 text-white rounded-[10px] cursor-pointer">
       문의하기
     </button>
   );
