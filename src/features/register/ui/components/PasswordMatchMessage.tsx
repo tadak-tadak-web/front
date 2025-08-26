73f9ffc6847eea2516d70cda2bb44ddfd63696d6
GuDoYoon(구도윤) <154503723+rhehfl@users.noreply.github.com>
2025-08-26 22:34:21 +0900

    (#16): 마진추가
    Co-authored-by: Kihyeon Jeong <dev.electrohyun@gmail.com>

--- src/features/register/ui/components/PasswordMatchMessage.tsx
+++ src/features/register/ui/components/PasswordMatchMessage.tsx
@@ -9,15 +9,15 @@ export default function PasswordMatchMessage({
 }: PasswordMatchMessageProps) {
   if (!checkPassword) {
     return (
-      <p className="text-sm opacity-0 select-none">
-        비밀번호가 일치하지 않습니다.
+      <p className="mt-2 text-sm opacity-0 select-none">
+        - 비밀번호가 일치하지 않습니다.
       </p>
     );
   }
   if (password !== checkPassword) {
     return (
-      <p className="text-red-500 text-sm">비밀번호가 일치하지 않습니다.</p>
+      <p className="mt-2 text-red-500 text-sm">- 비밀번호가 일치하지 않습니다.</p>
     );
   }
-  return <p className="text-green-500 text-sm">비밀번호가 일치합니다.</p>;
+  return <p className="mt-2 text-green-500 text-sm">- 비밀번호가 일치합니다.</p>;
 }
