b3f2405aea746dc9729b2adff3a35d7cb236b0ae
GuDoYoon <rhehfl418q@gmail.com>
2025-09-10 15:20:33 +0900

    (#25): 콘솔 제거

--- src/features/assignment/ui/FileSubmitForm.tsx
+++ src/features/assignment/ui/FileSubmitForm.tsx
@@ -5,14 +5,13 @@ import { useMutation } from '@tanstack/react-query';
 export default function FileSubmitForm() {
   const { mutate } = useMutation({
     mutationFn: createAssignment,
-    onSuccess: data => {
-      console.log('업로드 성공:', data);
-    },
-    onError: error => {
-      console.error('업로드 실패:', error);
-      alert('파일 업로드에 실패했습니다.');
-    },
   });
+
+  const handleUpload = (files: FileList) => {
+    for (const file of files) {
+      mutate(file);
+    }
+  };
 
   return (
     <form>
@@ -20,14 +19,7 @@ export default function FileSubmitForm() {
         <label className="block text-sm font-bold text-gray-700 mb-2">
           파일 업로드
         </label>
-        <DropZone
-          onFilesSelected={files => {
-            for (const file of files) {
-              console.log(file);
-              mutate(file);
-            }
-          }}
-        />
+        <DropZone onFilesSelected={handleUpload} />
       </div>
     </form>
   );
