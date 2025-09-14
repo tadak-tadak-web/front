4e3e70faf43fd7cab019dc1b39b4f0ceab13f025
electrohyun <dev.electrohyun@gmail.com>
2025-09-14 16:57:29 +0900

    (#28): 임시 mock 데이터 추가

---
+++ src/mocks/enrollment.mock.ts
@@ -0,0 +1,38 @@
+export const enrollmentMockData = [
+  {
+    id: 1,
+    imageUrl: 'https://picsum.photos/81',
+    title: '데이터베이스 시스템',
+    professor: '이민수',
+  },
+  {
+    id: 2,
+    imageUrl: 'https://picsum.photos/82',
+    title: '네트워크 보안',
+    professor: '박철수',
+  },
+  {
+    id: 3,
+    imageUrl: 'https://picsum.photos/83',
+    title: '컴퓨터 구조',
+    professor: '홍길동',
+  },
+  {
+    id: 4,
+    imageUrl: 'https://picsum.photos/84',
+    title: '프론트엔드 개발',
+    professor: '조은하',
+  },
+  {
+    id: 5,
+    imageUrl: 'https://picsum.photos/85',
+    title: '알고리즘 기초',
+    professor: '정현욱',
+  },
+  {
+    id: 6,
+    imageUrl: 'https://picsum.photos/86',
+    title: '컴퓨터 비전',
+    professor: '이화영',
+  },
+];
