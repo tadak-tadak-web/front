2c73e62bcbc314867b28b711030c18fed0c715bf
GuDoYoon <rhehfl418q@gmail.com>
2025-09-15 22:41:15 +0900

    (#26): 헤더 스타일링 추가

--- src/widgets/Header.tsx
+++ src/widgets/Header.tsx
@@ -1,31 +1,45 @@
 import { ArrowLeftIcon } from '@heroicons/react/24/outline';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate, useParams, useLocation } from 'react-router-dom';
+import clsx from 'clsx';
 
 export default function Header() {
+  const navigate = useNavigate();
+  const { id } = useParams();
+  const { pathname } = useLocation();
+  const handleBack = () => {
+    navigate(-1);
+  };
+
+  const lecturePath = `/lecture/${id}`;
+  const boardPath = `/lecture/${id}/board`;
+
   return (
     <header className="bg-white shadow-sm sticky top-0 z-10">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center py-3">
           <div className="flex items-center gap-4">
-            <button className="text-gray-600 hover:text-gray-900">
+            <button
+              onClick={handleBack}
+              className="text-gray-600 p-2 rounded-full hover:bg-gray-100"
+            >
               <ArrowLeftIcon className="h-6 w-6" />
             </button>
-            <h1 className="text-lg font-bold text-gray-800">기초프로그래밍</h1>
+            <h1 className="text-lg font-bold text-gray-800">
+              <Link to={lecturePath}>기초프로그래밍</Link>
+            </h1>
+            <nav className="hidden md:flex items-center gap-6 ml-20">
+              <Link
+                to={boardPath}
+                className={clsx('px-4', {
+                  'font-bold border-b-2 border-blue-600 pb-2':
+                    pathname === boardPath,
+                })}
+              >
+                게시판
+              </Link>
+            </nav>
           </div>
-          <nav className="hidden md:flex items-center gap-6">
-            <Link
-              to="#"
-              className="text-gray-600 hover:text-blue-600 font-medium"
-            >
-              강의
-            </Link>
-            <Link
-              to="#"
-              className="text-blue-600 border-b-2 border-blue-600 pb-3 font-bold"
-            >
-              Q&A
-            </Link>
-          </nav>
+
           <div className="w-10 h-10 bg-gray-200 rounded-full cursor-pointer"></div>
         </div>
       </div>
