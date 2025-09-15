63b3129afff01da15f95725200aa77744675b387
electrohyun <dev.electrohyun@gmail.com>
2025-09-15 16:14:03 +0900

    (#29): 리액트 쿼리를 이용한 api 함수 작성

---
+++ src/entities/enrollment/apis/getEnrollment.ts
@@ -0,0 +1,10 @@
+import { api } from '@/shared';
+import type { Enrollment } from '@/entities/enrollment';
+
+export const getEnrollment = async () => {
+  const response = await api
+    .get('enrollment')
+    .json<{ data: Enrollment[] | [] }>();
+
+  return response.data;
+};
