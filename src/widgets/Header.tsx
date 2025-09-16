19f1caea795522a7639bdfde30df7e2fdd484807
GuDoYoon <rhehfl418q@gmail.com>
2025-09-16 19:57:43 +0900

    (#26): 헤더 수정

--- src/widgets/Header.tsx
+++ src/widgets/Header.tsx
@@ -15,45 +15,6 @@ export default function Header() {
   const noticePath = `/lecture/${id}/notice`;
 
   return (
-    <header className="bg-white shadow-sm sticky top-0 z-10">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center py-3">
-          <div className="flex items-center gap-4">
-            <button
-              onClick={handleBack}
-              className="text-gray-600 p-2 rounded-full hover:bg-gray-100"
-            >
-              <ArrowLeftIcon className="h-6 w-6" />
-            </button>
-            <h1 className="text-lg font-bold text-gray-800">
-              <Link to={lecturePath}>기초프로그래밍</Link>
-            </h1>
-            <nav className="hidden md:flex items-center gap-6 ml-20">
-              <Link
-                to={boardPath}
-                className={clsx('px-4', {
-                  'font-bold border-b-2 border-blue-600 pb-2':
-                    pathname === boardPath,
-                })}
-              >
-                게시판
-              </Link>
-              <Link
-                to={boardPath}
-                className={clsx('px-4', {
-                  'font-bold border-b-2 border-blue-600 pb-2':
-                    pathname === noticePath,
-                })}
-              >
-                공지사항
-              </Link>
-            </nav>
-          </div>
-
-          <div className="w-10 h-10 bg-gray-200 rounded-full cursor-pointer"></div>
-        </div>
-      </div>
-    </header>
     <header className="h-16 bg-white shadow-sm sticky top-0 z-10">
       <div className="h-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="h-full flex justify-between items-center">
@@ -75,7 +36,7 @@ export default function Header() {
                   {
                     'font-bold border-b-2 border-blue-600':
                       pathname === boardPath,
-                  }
+                  },
                 )}
               >
                 게시판
@@ -87,7 +48,7 @@ export default function Header() {
                   {
                     'font-bold border-b-2 border-blue-600':
                       pathname === noticePath,
-                  }
+                  },
                 )}
               >
                 공지사항
